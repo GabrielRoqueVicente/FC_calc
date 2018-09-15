6faba9d43f62e865a315bd4af7848fc7c5ce74ab
Gabriel Roque Vicente <gabriel.vicente.59@gmail.com>
2018-09-15 17:46:07 +0200

    Numbers component finished

--- src/components/Numbers.js
+++ src/components/Numbers.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react'
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 
-//TODO Finir le compoment Numbers
-// import './Numbers.css'
-//
-const Numbers = ({ col, row }) => (
-    <div className="numbers">
-        <table>
+import './Numbers.css'
 
-        </table>
-    </div>
-);
+class Numbers extends Component {
 
-export default Numbers;
+    render() {
+        const buttons = this.props.buttons;
+
+        const rows = buttons > 0 && buttons > 10 ? buttons / 10 : 1;
+        const table = [];
+
+        for (let i = 0; i < rows; i++) {
+             const cols = [];
+             const nCols = i !== rows && buttons%10 === 0 ? 10 : buttons%10;
+            for (let j = 0; j < nCols ; j++) {
+                cols.push({value:i*10+j});
+            }
+            table.push(cols);
+        }
+
+        return (
+            <div className="numbers">
+                <table>
+                    <tbody>
+                        {table.map((row, i) =>
+                            <tr key={i} >
+                                {row.map((col, j) =>
+                                    <td key={col.value} className="btn" onClick={() => this.props.onClick(col.value)}>{col.value}</td>
+                                )}
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
+Numbers.propTypes ={
+    buttons: PropTypes.number.isRequired,
+    onClick: PropTypes.func.isRequired,
+
+};
+export default Numbers
